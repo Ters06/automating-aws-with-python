1fa99b56a2e52d3c74201a4b42c3a9e2b3090f8a
terence.leclercq <leclercq.terence@outlook.com>
2020-01-31 13:34:20 +0100

    Configure CloudFront for our website.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -16,12 +16,11 @@ It automates the process of deploying static web sites to AWS.
 import click
 import boto3
 
-from bucket import BucketManager
-from domain import DomainManager
-from certificate import CertificateManager
-from cdn import DistributionManager
-
-import util
+from webotron.bucket import BucketManager
+from webotron.domain import DomainManager
+from webotron.certificate import CertificateManager
+from webotron.cdn import DistributionManager
+from webotron import util
 
 session = None
 bucket_manager = None
